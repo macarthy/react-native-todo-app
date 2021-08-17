590c49b4d10484ad7232385f4c7ef1358414862b
Justin MacCarthy <justin@rubystars.biz>
2021-08-17 16:59:57 +0700

    A very simple todo app that uses localstoage to save todos. I want to see how difficult it is to transform into native apps for IOS and android

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App() {         
+
+    const [todos,setTodos] = useState([]);
+    const todoText=useRef();
+
+    useEffect(() => {
+        const existingTodos = localStorage.getItem('todos');
+        setTodos(existingTodos ? JSON.parse(existingTodos) : [])
+    },[]);
+
+    function addTodo(event){
+
+        event.preventDefault();
+        const next = [...todos,todoText.current.value];
+        setTodos(next)                                 ;
+        localStorage.setItem('todos',JSON.stringify(next));
+
+    } 
+
+
+    return (
+        <div className="App">
+            <h3>Todo app</h3>
+            <ul>
+                { todos.map(todo => (<li key={todo}> {todo} </li>)) }
+            </ul>
+            <form onSubmit={addTodo}>
+                <input ref={todoText} /> 
+                <input type='submit' value='Add todo' />
+            </form>
+        </div>
   );
 }
 
